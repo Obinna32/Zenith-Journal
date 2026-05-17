37ad4e3f37ef57595f89011acbc4876000b539e8
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-05-17 16:35:57 +0100

    feat: added habit model and mant-to-many relationship with entry

--- entries/models.py
+++ entries/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __sstr__(self):
+        return self.name
 class Entry(models.Model):
     MOOD_CHOICES = [
         ('happy', '😊 Happy'),
@@ -15,6 +22,8 @@ class Entry(models.Model):
     content = models.TextField()
     date_created = models.DateField()
     mood = models.CharField(max_length=20, choices=MOOD_CHOICES, default='neutral')
+
+    habits_completed = models.ManyToManyField(Habit, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
