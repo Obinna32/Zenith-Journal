34503fa799cfc53a1be311628d57a88891771a75
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-05-17 18:49:15 +0100

    feat: created basic frontend list view with Tailwind CSS

---
+++ entries/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import EntryListView
+
+urlpatterns = [
+    path('', EntryListView.as_view(), name='entry-list'),
+]
+
--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Entry
 
 # Create your views here.
+class EntryListView(ListView):
+    model = Entry
+    template_name = "entries/entry_list.html"
+    context_object_name = "entries"
+    ordering = ['date_created']
